eebfc2d10d2ee49955397a2bfeb392a960f69ce1
aaronzq <zhaoqiangwang@hust.edu.cn>
2019-05-26 19:57:52 -0700

    Update util.py
    Add augmentation when split dataset according to images
    revise crop so the image will be at the center

--- util_cnn/util.py
+++ util_cnn/util.py
@@ -99,9 +99,13 @@ def read_image(file_name_list, image_src_path, size, norm=True, crop=False):
         if crop:
             rmin, rmax, cmin, cmax = bbox(img)
             if rmax-rmin > cmax-cmin:
-                cmax = cmin + rmax-rmin
+                mid = 0.5*(cmin+cmax)
+                cmax = int(mid + 0.5*(rmax-rmin))
+                cmin = int(mid - 0.5*(rmax-rmin))
             else:
-                rmax = rmin + cmax-cmin
+                mid = 0.5*(rmin+rmax)
+                rmax = int(mid + 0.5*(cmax-cmin))
+                rmin = int(mid - 0.5*(cmax-cmin))
             img_rz = cv2.resize(img[rmin:rmax, cmin:cmax], (size[1],size[0]))
         else:
             img_rz = cv2.resize(img, (size[1],size[0]))
@@ -144,10 +148,24 @@ def read_data(label_path,image_folder_path,input_size,split_ratio,aug_rotate=6,s
         print('Split dataset according to Patients. Training : {} patients and {} images; Validation : {} patients and {} images'.format( 
             len(train_id),len(train_img),len(val_id),len(val_img) ))
     else:
-        train_img, val_img, train_label, val_label = train_test_split(image_list, 
+        train_img_temp, val_img_temp, train_label_temp, val_label_temp = train_test_split(image_list, 
         prog_list, test_size=split_ratio, random_state=42)
-        train_img = np.expand_dims(train_img, axis=-1)
-        val_img = np.expand_dims(val_img, axis=-1)
+
+        for i in range(len(train_img_temp)):
+            for ang in range(0,180,angleInc):
+                im_rot = rotate(train_img_temp[i], ang)
+                train_img.append( img_to_array(im_rot) )
+                train_label.append( train_label_temp[i] )      
+
+        for i in range(len(val_img_temp)):
+            for ang in range(0,180,angleInc):
+                im_rot = rotate(val_img_temp[i], ang)
+                val_img.append( img_to_array(im_rot) )
+                val_label.append( val_label_temp[i] )    
+
+
+        # train_img = np.expand_dims(train_img, axis=-1)
+        # val_img = np.expand_dims(val_img, axis=-1)
         print('Split dataset according to images. Training : {} images; Validation: {} images.'.format(
             len(train_img),len(val_img) ))
 
@@ -157,6 +175,8 @@ def read_data(label_path,image_folder_path,input_size,split_ratio,aug_rotate=6,s
     val_label_np = np.array(val_label)
 
     return train_img_np,train_label_np,val_img_np,val_label_np
+
+# def read_csv_kfold()
 
 def cosine_similarity(vec1, vec2):
     return np.dot(vec1, vec2) / (np.sqrt(np.dot(vec1, vec1)) * np.sqrt(np.dot(vec2, vec2)))
@@ -174,7 +194,7 @@ if __name__ == "__main__":
     # read_data(csvPath,imagePath,inputSize,0.3,split_by_id=True,normalize=True,crop_image=False)
 
     train_img,train_label,val_img,val_label = read_data(csvPath,
-        imagePath,inputSize,0.2,split_by_id=True,normalize=True,crop_image=False)
+        imagePath,inputSize,0.2,split_by_id=False,normalize=True,crop_image=True)
         
     pass
     # print(type(train_img[5])) 
